9ea8573ee3b9d96373185a2f334e3e0b2da95081
duncanb730 <duncanb730@gmail.com>
2016-05-25 16:11:33 -0700

    Added Photo

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,17 @@
-$("#main").append("Brent Duncan");
+var name = "Brent Duncan";
+var formattedName = HTMLheaderName.replace("%data%", name)
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+
+var Bio = {
+	// <img src="images/headshot.jpg" alt="me">
+	"name" : "Brent",
+	"role" : "Web Developer",
+	"Welcome into my Skull."
+	"skills" : "HTML/CSS Programmer" ,"JS" ,"Insanity"
+}
+$("#header").append(formattedBio);
